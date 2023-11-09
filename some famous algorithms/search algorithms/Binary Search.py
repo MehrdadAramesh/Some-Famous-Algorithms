86da075ef465d7907b84009e8ff81f376620eea8
MehrdadAramesh <mehrdad.aramesh2002@gmail.com>
2023-11-09 22:27:38 +0330

    little change in binary search

--- some famous algorithms/search algorithms/Binary Search.py
+++ some famous algorithms/search algorithms/Binary Search.py
@@ -1,12 +1,15 @@
 
+import math
+
 #the given array must be sorted!
 
 def binary_search(arr,element,low,high):
 
-    import math
 
     ind = math.floor((high+low)/2)
 
     if arr[ind] == element : return ind
+
     elif arr[ind]<element : return binary_search(arr,element,ind,high)
+    
     else : return binary_search(arr,element,0,ind)
